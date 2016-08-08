f41cbd13e7d7534e843ac4558a5a049adadb897e
ravigumpu <gumpuravi@gmail.com>
2016-08-08 14:41:43 +1000

    Jenkins GitHub Push Test

---
+++ src/test/java/com/jenkins/maven/quickstart/JenkinsPushGitHubTest.java
@@ -0,0 +1,38 @@
+package com.jenkins.maven.quickstart;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JenkinsPushGitHubTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JenkinsPushGitHubTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JenkinsPushGitHubTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
